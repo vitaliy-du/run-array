205e5dafa2cfcdeaccff34aa35b3fbd79b6e224b
Vitaliy Dyukar <vitaliy.du@yandex.ru>
2020-09-14 13:02:11 +0300

    Added functions
    - added asyncEvery
    - added asyncMap
    - added asyncReduce
    - added asyncReduceRight
    - added asyncSome

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,90 @@
 declare namespace DaddyArray {
+    type Result<T> = {
+        result: T;
+        success: boolean;
+    }
+
     /**
-     * Asynchronously performs the specified action for each element in an array.
+     * Asynchronously determines whether all the members of an array satisfy the specified test.
      * The length of chunk to process is calculated automatically to avoid slowing down the GUI.
      *
-     * @param arr Array;
-     * @param more
-     *   A callback function that called one time for each element in the array.
-     *   If return *true* enumeration will continue, overwise cancelled.
+     * @param arr Array.
+     * @param more Called one time for each element in the array until returns a value which is coercible to the Boolean
+     * value false, or until the end of the array. To stop enumeration call *stop*.
      *
-     * @returns Promise resolves *true* after successful enumeration, overwise *false*.
+     * @returns Promise resolves *{result: true or false, success: true or false if stop enumeration}*.
      */
-    function asyncForEach<T = any>(arr: T[], more: (x: T, i: number, arr: T[]) => boolean): Promise<boolean>;
+    function asyncEvery<T = any>(arr: T[],
+        more: (x: T, i: number, arr: T[], stop?: () => void) => void): Promise<Result<boolean>>;
+
+    /**
+     * Asynchronously performs the specified *more* action for each element in an array.
+     * The length of chunk to process is calculated automatically to avoid slowing down the GUI.
+     *
+     * @param arr Array.
+     * @param more Called one time for each element in the array. To stop enumeration call *stop*.
+     *
+     * @returns Promise resolves *{result: arr, success: true or false if stop enumeration}*.
+     */
+    function asyncForEach<T = any>(arr: T[],
+        more: (x: T, i: number, arr: T[], stop?: () => void) => void): Promise<Result<T[]>>;
+
+    /**
+     * Asynchronously calls the specified *more* function on each element of an array, and returns an array that
+     * contains the results. The length of chunk to process is calculated automatically to avoid slowing down the GUI.
+     *
+     * @param arr Array.
+     * @param more Called one time for each element in the array. To stop enumeration call *stop*.
+     *
+     * @returns Promise resolves *{result: new array, success: true or false if stop enumeration}*.
+     */
+    function asyncMap<T = any, R = any>(arr: T[],
+        more: (x: T, i: number, arr: T[], stop?: () => void) => void): Promise<Result<R>>;
+
+    /**
+     * Asynchronously calls the specified *more* function for all the elements in an array.
+     * The return value of the *more* function is the accumulated result, and is provided as an argument in the next
+     * call to the *more* function. The length of chunk to process is calculated automatically to avoid slowing down
+     * the GUI.
+     *
+     * @param arr Array.
+     * @param more Called one time for each element in the array. To stop enumeration call *stop*.
+     * @param init If value is specified, it is used as the initial value to start the accumulation.
+     * The first call to the *more* function provides this value as an argument instead of an first array value.
+     *
+     * @returns Promise resolves *{result: accumulated result, success: true or false if stop enumeration}*.
+     */
+    function asyncReduce<T = any, R = any>(arr: T[],
+        more: (r: R, x: T, i: number, arr: T[], stop?: () => void) => void, init: R): Promise<Result<R>>;
+
+    /**
+     * Asynchronously calls the specified *more* function for all the elements in an array, in descending order.
+     * The return value of the *more* function is the accumulated result, and is provided as an argument in the next
+     * call to the *more* function. The length of chunk to process is calculated automatically to avoid slowing down
+     * the GUI.
+     *
+     * @param arr Array.
+     * @param more Called one time for each element in the array. To stop enumeration call *stop*.
+     * @param init If value is specified, it is used as the initial value to start the accumulation.
+     * The first call to the *more* function provides this value as an argument instead of an first array value.
+     *
+     * @returns Promise resolves *{result: accumulated result, success: true or false if stop enumeration}*.
+     */
+    function asyncReduceRight<T = any, R = any>(arr: T[],
+        more: (r: R, x: T, i: number, arr: T[], stop?: () => void) => void, init: R): Promise<Result<R>>;
+
+    /**
+     * Asynchronously determines whether the specified *more* function returns true for any element of an array.
+     * The length of chunk to process is calculated automatically to avoid slowing down the GUI.
+     *
+     * @param arr Array.
+     * @param more Called one time for each element in the array until returns a value which is coercible to the Boolean
+     * value true, or until the end of the array. To stop enumeration call *stop*.
+     *
+     * @returns Promise resolves *{result: true or false, success: true or false if stop enumeration}*.
+     */
+    function asyncSome<T = any>(arr: T[],
+        more: (x: T, i: number, arr: T[], stop?: () => void) => void): Promise<Result<boolean>>;
 }
 
 export = DaddyArray;
--- index.js
+++ index.js
@@ -1,29 +1,77 @@
-export const asyncForEach = (arr, more) => (
-    new Promise(resolve => {
-        if (!arr || !arr.length || !more) {
-            resolve(true);
+function asyncEnum(arr, more, resolve, defResult, initResult, enumFrom, getBreak, getResult) {
+    if (!arr || !arr.length || !more) {
+        resolve({result: defResult, success: true});
+        return;
+    }
+
+    var doFrom = enumFrom;
+    var doLen = 1;
+    var doStop = false;
+    var result = initResult;
+    var stop = () => doStop = true;
+    var forEach = () => {
+        var startTime = Date.now();
+        for (var i = doFrom; i < Math.min(doFrom + doLen, arr.length); i++) {
+            result = getResult(stop, i, result);
+            if ((getBreak && getBreak(result)) || doStop) break;
+        }
+        doFrom += doLen;
+        if ((doFrom >= arr.length) || doStop) {
+            resolve({result, success: !doStop});
             return;
         }
+        doLen = (Date.now() - startTime < 34) ? (doLen << 1) : Math.max(doLen >> 1, 1);
+        setTimeout(forEach, 0);
+    };
 
-        let doFrom = 0;
-        let doLen = 1;
-        const forEach = () => {
-            let cancel = false;
-            const startTime = Date.now();
-            for (let i = doFrom; i < Math.min(doFrom + doLen, arr.length); i++) {
-                if (more(arr[i], i, arr)) continue;
-                cancel = true;
-                break;
-            }
-            doFrom += doLen;
-            if ((doFrom >= arr.length) || cancel) {
-                resolve(!cancel);
-                return;
-            }
-            doLen = (Date.now() - startTime < 34) ? (doLen << 1) : Math.max(doLen >> 1, 1);
-            setTimeout(forEach, 0);
-        };
+    forEach();
+}
 
-        forEach();
-    })
-);
+export function asyncEvery(arr, more) {
+    return new Promise(resolve => {
+        asyncEnum(arr, more, resolve, true, true, 0, r => !r, (s, i) => more(arr[i], i, arr, s));
+    });
+}
+
+export function asyncForEach(arr, more) {
+    return new Promise(resolve => {
+        asyncEnum(arr, more, resolve, arr, arr, 0, null, (s, i) => more(arr[i], i, arr, s));
+    });
+}
+
+export function asyncMap(arr, more) {
+    return new Promise(resolve => {
+        asyncEnum(arr, more, resolve, arr, [], 0, null, (s, i, r) => {
+            r[i] = more(arr[i], i, arr, s);
+            return r;
+        });
+    });
+}
+
+export function asyncReduce(arr, more, init) {
+    return new Promise(resolve => {
+        var defInit = arr.length && (arguments.length < 3);
+        asyncEnum(arr, more, resolve, arr, defInit ? 1 : 0, defInit ? arr[0] : init, null, (s, i, r) => {
+            r = more(r, arr[i], i, arr, s);
+            return r;
+        });
+    });
+}
+
+export function asyncReduceRight(arr, more, init) {
+    return new Promise(resolve => {
+        var arrLen = arr && arr.length;
+        var defInit = arrLen && (arguments.length < 3);
+        asyncEnum(arr, more, resolve, arr, defInit ? 1 : 0, defInit ? arr[0] : init, null, (s, i, r) => {
+            var index = arrLen - i - 1;
+            r = more(r, arr[index], index, arr, s);
+            return r;
+        });
+    });
+}
+
+export function asyncSome(arr, more) {
+    return new Promise(resolve => {
+        asyncEnum(arr, more, resolve, false, false, 0, r => r, (s, i) => more(arr[i], i, arr, s));
+    });
+}
