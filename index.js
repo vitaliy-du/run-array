8cb723cd091ce09b034ed43c50301c8438fca775
Vitaliy Dyukar <vitaliy.du@yandex.ru>
2021-02-24 19:32:17 +0300

    Improvements… version 3.1.3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function asyncEnum(arr, more, from, done, def, init, get) {
+function asyncEnum(arr, next, from, done, def, init, get) {
 	return new Promise(function (finish) {
-		if (!arr || !arr.length || !more) {
+		if (!arr || !arr.length || !next) {
 			finish({result: def, success: true});
 			return;
 		}
@@ -35,9 +35,9 @@ function asyncEnum(arr, more, from, done, def, init, get) {
 	});
 }
 
-function prlEnum(prl, arr, more, from, done, def, init, exec, get) {
+function prlEnum(prl, arr, next, from, done, def, init, exec, get) {
 	return new Promise(function (finish) {
-		if (!arr || !arr.length || !more) {
+		if (!arr || !arr.length || !next) {
 			finish({result: def, success: true});
 			return;
 		}
@@ -85,44 +85,44 @@ function prlEnum(prl, arr, more, from, done, def, init, exec, get) {
 	});
 }
 
-export function asyncEvery(arr, more) {
-	return asyncEnum(arr, more,  0, r => !r, true, true,
-		(s, i) => !!more(arr[i], i, arr, s));
+export function asyncEvery(arr, next) {
+	return asyncEnum(arr, next,  0, r => !r, true, true,
+		(s, i) => !!next(arr[i], i, arr, s));
 }
 
-export function asyncFilter(arr, more) {
-	return asyncEnum(arr, more, 0, null, [], [],
+export function asyncFilter(arr, next) {
+	return asyncEnum(arr, next, 0, null, [], [],
 		(s, i, r) => {
 			var x = arr[i];
-			more(x, i, arr, s) && r.push(x);
+			next(x, i, arr, s) && r.push(x);
 			return r;
 		});
 }
 
-export function asyncFind(arr, more) {
-	return asyncEnum(arr, more, 0, r => r !== undefined, undefined, undefined,
-		(s, i, r) => more(arr[i], i, arr, s) ? arr[i] : r);
+export function asyncFind(arr, next) {
+	return asyncEnum(arr, next, 0, r => r !== undefined, undefined, undefined,
+		(s, i, r) => next(arr[i], i, arr, s) ? arr[i] : r);
 }
 
-export function asyncFindIndex(arr, more, fromIndex) {
-	return asyncEnum(arr, more, fromIndex || 0, r => r > -1, -1, -1,
-		(s, i, r) => more(arr[i], i, arr, s) ? i : r);
+export function asyncFindIndex(arr, next, fromIndex) {
+	return asyncEnum(arr, next, fromIndex || 0, r => r > -1, -1, -1,
+		(s, i, r) => next(arr[i], i, arr, s) ? i : r);
 }
 
-export function asyncForEach(arr, more) {
-	return asyncEnum(arr, more, 0, null, arr, arr,
+export function asyncForEach(arr, next) {
+	return asyncEnum(arr, next, 0, null, arr, arr,
 		(s, i, r) => {
-			more(arr[i], i, arr, s);
+			next(arr[i], i, arr, s);
 			return r;
 		});
 }
 
-export function asyncIndexOf(arr, value, fromIndex) {
+export function asyncIndexBy(arr, value, fromIndex) {
 	return asyncEnum(arr, true, fromIndex || 0, r => r > -1, -1, -1,
 		(s, i, r) => arr[i] === value ? i : r);
 }
 
-export function asyncLastIndexOf(arr, value, fromIndex) {
+export function asyncLastIndexBy(arr, value, fromIndex) {
 	var arrLen = arr && arr.length || 0;
 	return asyncEnum(arr, true, -fromIndex || 0, r => r > -1, -1, -1,
 		(s, i, r) => {
@@ -131,88 +131,88 @@ export function asyncLastIndexOf(arr, value, fromIndex) {
 		});
 }
 
-export function asyncMap(arr, more) {
-	return asyncEnum(arr, more, arr, 0, null, new Array(arr.length),
+export function asyncMap(arr, next) {
+	return asyncEnum(arr, next, arr, 0, null, new Array(arr.length),
 		(s, i, r) => {
-			r[i] = more(arr[i], i, arr, s);
+			r[i] = next(arr[i], i, arr, s);
 			return r;
 		});
 }
 
-export function asyncReduce(arr, more, init) {
+export function asyncReduce(arr, next, init) {
 	var defInit = arr.length && (arguments.length < 3);
-	return asyncEnum(arr, more, defInit ? 1 : 0, null, undefined, defInit ? arr[0] : init,
-		(s, i, r) => more(r, arr[i], i, arr, s));
+	return asyncEnum(arr, next, defInit ? 1 : 0, null, undefined, defInit ? arr[0] : init,
+		(s, i, r) => next(r, arr[i], i, arr, s));
 }
 
-export function asyncReduceRight(arr, more, init) {
+export function asyncReduceRight(arr, next, init) {
 	var arrLen = arr && arr.length || 0;
 	var defInit = arrLen && (arguments.length < 3);
-	return asyncEnum(arr, more, defInit ? 1 : 0, null, undefined, defInit ? arr[0] : init,
+	return asyncEnum(arr, next, defInit ? 1 : 0, null, undefined, defInit ? arr[0] : init,
 		(s, i, r) => {
 			var index = arrLen - i - 1;
-			return more(r, arr[index], index, arr, s);
+			return next(r, arr[index], index, arr, s);
 		});
 }
 
-export function asyncSome(arr, more) {
-	return asyncEnum(arr, more, 0, r => r, false, false,
-		(s, i) => !!more(arr[i], i, arr, s));
+export function asyncSome(arr, next) {
+	return asyncEnum(arr, next, 0, r => r, false, false,
+		(s, i) => !!next(arr[i], i, arr, s));
 }
 
-export function prlEvery(prl, arr, more) {
-	return prlEnum(prl, arr, more, 0, r => !r, true, true,
-		(done, s, i) => more(done, arr[i], i, arr, s), x => !!x);
+export function prlEvery(prl, arr, next) {
+	return prlEnum(prl, arr, next, 0, r => !r, true, true,
+		(done, s, i) => next(done, arr[i], i, arr, s), x => !!x);
 }
 
-export function prlFilter(prl, arr, more) {
-	return prlEnum(prl, arr, more, 0, null, [], [],
-		(done, s, i) => more(done, arr[i], i, arr, s), (x, i, r) => {
+export function prlFilter(prl, arr, next) {
+	return prlEnum(prl, arr, next, 0, null, [], [],
+		(done, s, i) => next(done, arr[i], i, arr, s), (x, i, r) => {
 			x && r.push(arr[i]);
 			return r;
 		});
 }
 
-export function prlFind(prl, arr, more) {
-	return prlEnum(prl, arr, more, 0, r => r !== undefined, undefined, undefined,
-		(done, s, i) => more(done, arr[i], i, arr, s), (x, i, r) => x ? arr[i] : r);
+export function prlFind(prl, arr, next) {
+	return prlEnum(prl, arr, next, 0, r => r !== undefined, undefined, undefined,
+		(done, s, i) => next(done, arr[i], i, arr, s), (x, i, r) => x ? arr[i] : r);
 }
 
-export function prlFindIndex(prl, arr, more, fromIndex) {
-	return prlEnum(prl, arr, more, fromIndex || 0, r => r > -1, -1, -1,
-		(done, s, i) => more(done, arr[i], i, arr, s), (x, i, r) => x ? i : r);
+export function prlFindIndex(prl, arr, next, fromIndex) {
+	return prlEnum(prl, arr, next, fromIndex || 0, r => r > -1, -1, -1,
+		(done, s, i) => next(done, arr[i], i, arr, s), (x, i, r) => x ? i : r);
 }
 
-export function prlForEach(prl, arr, more) {
-	return prlEnum(prl, arr, more, 0, null, arr, arr,
-		(done, s, i) => more(done, arr[i], i, arr, s), () => arr);
+export function prlForEach(prl, arr, next) {
+	return prlEnum(prl, arr, next, 0, null, arr, arr,
+		(done, s, i) => next(done, arr[i], i, arr, s), () => arr);
 }
 
-export function prlMap(prl, arr, more) {
-	return prlEnum(prl, arr, more, 0, null, arr, new Array(arr.length),
-		(done, s, i) => more(done, arr[i], i, arr, s), (x, i, r) => {
+export function prlMap(prl, arr, next) {
+	return prlEnum(prl, arr, next, 0, null, arr, new Array(arr.length),
+		(done, s, i) => next(done, arr[i], i, arr, s), (x, i, r) => {
 			r[i] = x;
 			return r;
 		});
 }
 
-export function prlReduce(prl, arr, more, init) {
+export function prlReduce(prl, arr, next, init) {
 	var defInit = arr.length && (arguments.length < 3);
-	return prlEnum(prl, arr, more, defInit ? 1 : 0, null, undefined, defInit ? arr[0] : init,
-		(done, s, i, r) => more(done, r, arr[i], i, arr, s), (x, i, r) => r);
+	return prlEnum(prl, arr, next, defInit ? 1 : 0, null, undefined, defInit ? arr[0] : init,
+		(done, s, i, r) => next(done, r, arr[i], i, arr, s), (x, i, r) => r);
 }
 
-export function prlReduceRight(prl, arr, more, init) {
+export function prlReduceRight(prl, arr, next, init) {
 	var arrLen = arr && arr.length || 0;
 	var defInit = arrLen && (arguments.length < 3);
-	return prlEnum(prl, arr, more, defInit ? 1 : 0, null, undefined, defInit ? arr[0] : init,
+	return prlEnum(prl, arr, next, defInit ? 1 : 0, null, undefined, defInit ? arr[0] : init,
 		(done, s, i, r) => {
 			var index = arrLen - i - 1;
-			more(done, r, arr[index], index, arr, s);
+			next(done, r, arr[index], index, arr, s);
 		}, (x, i, r) => r);
 }
 
-export function prlSome(prl, arr, more) {
-	return prlEnum(prl, arr, more, 0, r => r, false, false,
-		(done, s, i) => more(done, arr[i], i, arr, s), x => !!x);
+export function prlSome(prl, arr, next) {
+	return prlEnum(prl, arr, next, 0, r => r, false, false,
+		(done, s, i) => next(done, arr[i], i, arr, s), x => !!x);
 }
